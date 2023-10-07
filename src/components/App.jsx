689d94f0d887dcf4cebd6f90807d431be69b1582
Andrii Kurshatsov <andreswebit@ukr.net>
2023-10-07 13:16:51 +0300

    Add phonebook redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { ToastContainer } from 'react-toastify'; // відображення сповіщень
+import 'react-toastify/dist/ReactToastify.css'; // стилі для сповіщень
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { MainHeader, SecondaryHeader, Section } from './App.styled'; // стилі для компонентів
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <MainHeader>Phonebook</MainHeader>
+
+      <ContactForm />
+      <SecondaryHeader>Contacts</SecondaryHeader>
+      
+      <Filter />
+      <ContactList />
+      <ToastContainer position="top-center" /> {/* відображення сповіщень */}
+    </Section>
   );
 };
